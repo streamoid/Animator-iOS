6e3af10deeb413c4b2b3081a042acb9ce6ab1cb1
Vengal <vengal@streamoid.com>
2017-01-18 15:57:16 +0530

    Added example project

---
+++ AnimatorExample/AnimatorExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimatorExample
+//
+//  Created by Vengal on 18/01/17.
+//  Copyright © 2017 Streamoid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimatorExample/AnimatorExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimatorExample
+//
+//  Created by Vengal on 18/01/17.
+//  Copyright © 2017 Streamoid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
